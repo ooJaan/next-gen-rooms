d5fd1fd1eb64484f172393bada7cee5bbc302a6c
Jan Schabauer <jan@schabauer.org>
2025-03-18 22:25:54 +0100

    fetching rommlist from api

--- frontend/src/pages/RoomList.js
+++ frontend/src/pages/RoomList.js
@@ -1,13 +1,15 @@
 import '../css/App.css';
 import '../css/Table.css';
 
-import data from '../data/rooms.json'
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from 'react-router-dom';
+
 
 import Modal from '../comps/Modal';
 import Table from '../comps/Table';
+
+import { useApi } from "../helpers/api";
 
 
 const BuchungsButton = ({ openModal, raumnr }) => {
@@ -36,11 +38,32 @@ const ModalContent = ({ modalData }) => {
 
 
 
-const RoomList = () => {
+const RoomList =  () => {
+    const [ rooms, setRooms] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const { fetchWithAuth } = useApi();
+
     const [modalData, setModalData] = useState(null);
     const openModal = (data) => {
         setModalData(data); // Store passed data in state
     };
+    useEffect(() => {
+        const fetchRooms = async () => {
+            try {
+                const response = await fetchWithAuth("room"); // Replace with actual endpoint
+                console.log(response)
+                setRooms(response); // Store the response in state
+                setLoading(false);
+            } catch (err) {
+                console.error("Error fetching rooms:", err);
+                setError("Failed to load rooms.");
+                setLoading(false);
+            }
+        };
+
+        fetchRooms();
+    },[]);
 
     const closeModal = () => {
         setModalData(null); // Clear modal data to close
@@ -50,6 +73,8 @@ const RoomList = () => {
         // This will navigate to the "/about" page
         navigate('/overview/'+ id);
     };
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>{error}</p>;
     return (
         <div>
             <Table
@@ -59,16 +84,18 @@ const RoomList = () => {
                         <th>Name</th>
                         <th>RaumNr</th>
                         <th>Kapazität</th>
+                        <th>Max Buchungsdauer</th>
                         <th className="grow">Austattung</th>
                         <th></th>
                     </tr>
                 }
-                body={data.map((row) => (
-                    <tr key={row.RaumNr}>
+                body={Object.entries(rooms).map(([key, row]) => (
+                    <tr key={key} name={key}>
                         <td className={row.status}><div>●</div></td>
                         <td><Link to={`/overview/${row.roomId}`}>{row.name}</Link></td>
                         <td>{row.number}</td>
                         <td>{row.capacity}</td>
+                        <td>{row.maxDuration}</td>
                         <td>{row.Auststattung}</td>
                         <td><BuchungsButton openModal={openModal} raumnr={row.roomId}></BuchungsButton></td>
                     </tr>
