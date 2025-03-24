496c03709172bf4d57e0b32876b246a90292e64d
Jan Schabauer <jan@schabauer.org>
2025-03-24 22:33:13 +0100

    api cleaning up

--- frontend/src/helpers/api.js
+++ frontend/src/helpers/api.js
@@ -7,32 +7,54 @@ import config from "../config";
 export const useApi = () => {
     const { token, refreshAccessToken } = useContext(AuthContext);
 
-        const fetchWithAuth = async (endpoint, options = {}) => {
-            const url = `${config.apiUrl}/${endpoint}`
-            console.log("fetching data from ", url)
-            let response = await fetch(url, {
-              ...options,
-              headers: {
+    const reqWithAuth = async (endpoint, method="GET", data) => {
+        const url = `${config.apiUrl}/${endpoint}`
+        console.log(`${method} to ${url} with data: ${data}`)
+        const req = {
+            method: method,
+            headers: {
                 "Content-Type": "application/json", // Ensure Content-Type is always included
                 "Authorization": `Bearer ${token}`, // Include token
-                ...options.headers, // Allow custom headers
-              },
-            });
-
-        if (response.status === 401) {
-            await refreshAccessToken();
-            response = await fetch(url, {
-                ...options,
-                headers: {
-                    ...options.headers,
-                    Authorization: `Bearer ${token}`, // Get new token
-                },
-            });
+            },
+            body: JSON.stringify(data)
         }
-        return response.json();
+        let response = await fetch(url, req);
+        if (response.status === 401) {
+            console.log("unauthorized --> trying to refresh the token")
+            await refreshAccessToken();
+            response = await fetch(url, req);
+        }
+        if (!response.ok) {
+            return {}
+        }
+        try {
+            const jsonData = await response.json();
+            return jsonData; 
+        } catch (error) {
+            return response;
+        }
+    }
 
+    const fetchWithAuth = async (endpoint) => {
+        const resp = await reqWithAuth(endpoint, "GET")
+        console.log(resp)
+        return resp;
     };
+    const postWithAuth = async (endpoint, data) => {
+        const resp = await reqWithAuth(endpoint, "POST", data)
+        return resp;
+    }
 
+    const deleteWithAuth = async(endpoint, data) => {
+        const resp = await reqWithAuth(endpoint, "DELETE", data)
+        return resp;
+    }
+    const patchWithAuth = async(endpoint, data) => {
+        const resp = await reqWithAuth(endpoint, "PATCH", data)
+        return resp
+    }
 
-    return { fetchWithAuth };
+    
+
+    return { fetchWithAuth, postWithAuth, deleteWithAuth, patchWithAuth};
 };
