cd45b69107ff0d8d23afcd6c3835f50c9071d78d
BaumiZockt <oliver.osterhase@gmail.com>
2025-04-08 08:53:26 +0200

    add condition to show delete button

--- frontend/src/pages/RoomOverview.js
+++ frontend/src/pages/RoomOverview.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import { useParams, useNavigate } from 'react-router-dom';
 
 import { RoomContext } from "../provider/RoomStatus.tsx";
+import { AuthContext } from "../provider/AuthProvider.js";
 import { useModify } from '../helpers/Modify.ts';
 
 
@@ -9,18 +10,15 @@ import { useApi } from '../helpers/api';
 import useDate from '../helpers/Date';
 import Table from "../comps/Table";
 import Loading from "../comps/Loading";
-import BookingDialog from '../comps/BookDialog';
-
 import '../css/RoomOverview.css'
-import '../css/classes.css'
 
 const RoomOverview = () => {
     const { rooms, roomLoading, users, userLoading, status, statusLoading, assets, assetLoading, typeAssets, typeAssetsLoading } = useContext(RoomContext);
     const { id } = useParams();
-    const [bookDialogClosed, setBookDialogClosed] = useState(true)
     const { formatDate, formatTime } = useDate();
     const navigate = useNavigate();
     const { deleteBooking } = useModify(id);
+    const { c_userId } = useContext(AuthContext);
 
     const equipmentColumns = [
         { key: 'name', label: 'Name', sortable: true },
@@ -41,9 +39,13 @@ const RoomOverview = () => {
             key: 'action',
             label: 'Action',
             sortable: false,
-            render: (row) => (
-                <button onClick={() => deleteBooking(row.bookingId) || null}>Löschen</button>
-            )        
+            render: (row) => {
+               if(row.userId === c_userId){
+                    return <button onClick={() => deleteBooking(row.bookingId)}>Löschen</button>
+               }else{
+                    return <button disabled={true}>Löschen</button>
+               }
+            }
         }
     ];
 
@@ -108,14 +110,7 @@ const RoomOverview = () => {
                     )}
                 </div>
                 <div>
-                    <div className="flex-horizontal justify-space-between">
-                        <div >
-                            <h1>Buchungen</h1>
-                        </div>
-                        <div>
-                            <button onClick={() => setBookDialogClosed(false)}>Buchen</button>
-                        </div>
-                    </div>
+                    <h1>Buchungen</h1>
                     {!roomLoading ? (
                         <Table
                             head={
@@ -136,12 +131,6 @@ const RoomOverview = () => {
 
                 </div>
             </div>
-
-            <BookingDialog
-                roomData={rooms[id]}
-                setClosed={setBookDialogClosed}
-                modalClosed={bookDialogClosed}
-            />
         </div>
     )
 }
